f22267671c9b66a46b95e5e122177a8b8aebd841
sheila <sheilafundora04@gmail.com>
2023-09-24 18:03:04 -0400

    pages and styles

--- sgsdph_frontend/app/dashboard/page.jsx
+++ sgsdph_frontend/app/dashboard/page.jsx
@@ -1,9 +1,37 @@
+'use client'
 import * as React from 'react';
+import Button from '@mui/material/Button';
+import CardDescription from "../../components/CardDescription";
+import BasicTable from "../../components/TableMine";
+import CreateSolicitudModal from "../../components/CreateSolicitudModal";
+import {useState} from "react";
+import Typography from "@mui/material/Typography";
 
-export default function HomePage() {
+export default function BasicCard() {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
-        <div> Home </div>
+        <div>
+            <CardDescription />
+
+            <div className='d-flex justify-content-end m-2'>
+                <Button variant="contained" onClick={handleClickOpen}>+ Agregar Solicitud</Button>
+            </div>
+
+            <CreateSolicitudModal isOpen={open} handleClose={handleClose} setOpen={setOpen} />
+
+            <Typography className={'text-secondary my-2 ms-2'}>Listado de Soliciudes de Dietas</Typography>
+            <BasicTable />
+
+        </div>
+
     );
 }
-
--- sgsdph_frontend/app/dashboard/solicitudes_canceladas/page.jsx
+++ sgsdph_frontend/app/dashboard/solicitudes_canceladas/page.jsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
+import BasicTable from "../../../components/TableMine";
 
 export default function SolicitudesCanceladas() {
 
     return (
-        <div> Solicitudes Canceladas</div>
+        <div>
+            <p className={'text-secondary my-3 ms-2'}>Listado de modelos cancelados</p>
+
+            <BasicTable />
+
+            <p>La tabla lleva previsualizar</p>
+
+        </div>
     );
 }
