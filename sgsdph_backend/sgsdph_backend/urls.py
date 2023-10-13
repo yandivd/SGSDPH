2b8f5e36cedc879bb6f7f383b51e1d9e3df9d621
yandivd <yandivd@gmail.com>
2023-10-13 10:43:47 -0400

    ✨ feat(solicitud): models
    modelo de  solicitud
    
    el antiguo, falta fix

--- sgsdph_backend/sgsdph_backend/urls.py
+++ sgsdph_backend/sgsdph_backend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from autentic.views import LoginAPI, LogoutAPI
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('logout/', LogoutAPI.as_view(), name='logout'),
     path('', include('autentic.api.urls')),
 ]
+if settings.DEBUG: #revisar q este en desarrollo
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- sgsdph_backend/sistema/models.py
+++ sgsdph_backend/sistema/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from autentic.models import Trabajador, Unidad_Organizativa, Centro_Costo, Cargo_al_Presupuesto
 
 # Create your models here.
+
+class Solicitud(models.Model):
+    numero=models.IntegerField()
+    solicitante=models.ForeignKey(Trabajador, on_delete=models.CASCADE, related_name='Solicitante')
+    trabajador=models.ForeignKey(Trabajador, on_delete=models.CASCADE)
+    unidad_organizativa=models.ForeignKey(Unidad_Organizativa, on_delete=models.CASCADE)
+    c_contable=models.ForeignKey(Centro_Costo, on_delete=models.CASCADE)
+    provincia=models.CharField(max_length=50)
+    origen=models.CharField(max_length=50)
+    prov_destino=models.CharField(max_length=50)
+    destino=models.CharField(max_length=50)
+    regreso=models.CharField(max_length=50)
+    fecha_inicio=models.DateField()
+    fecha_final=models.DateField()
+    #nuevos models add
+    parleg=models.ForeignKey(Trabajador, on_delete=models.CASCADE, related_name='Trabajador_parleg', null=True, blank=True)
+    cargo_presupuesto=models.ForeignKey(Cargo_al_Presupuesto, on_delete=models.CASCADE)
+    autoriza=models.ForeignKey(Trabajador, on_delete=models.CASCADE, related_name='Autoriza')
+    estado=models.CharField(max_length=200)
+    observaciones=models.CharField(max_length=500, blank=True, null=True)
+    labor=models.CharField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.numero)
+
+    class Meta:
+        verbose_name="Solicitud"
+        verbose_name_plural="Solicitudes"
