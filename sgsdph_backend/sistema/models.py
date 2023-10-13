a5daf6c5ed5a52d74123fb4341abab2b7f153bf9
yandivd <yandivd@gmail.com>
2023-10-13 16:15:03 -0400

    ✨ feat(solicitudes): api
    apis de las solicitudes

--- sgsdph_backend/sistema/models.py
+++ sgsdph_backend/sistema/models.py
@@ -4,19 +4,29 @@ from autentic.models import Trabajador, Unidad_Organizativa, Centro_Costo, Cargo
 # Create your models here.
 
 class Solicitud(models.Model):
-    numero=models.IntegerField()
-    solicitante=models.ForeignKey(Trabajador, on_delete=models.CASCADE, related_name='Solicitante')
-    trabajador=models.ForeignKey(Trabajador, on_delete=models.CASCADE)
-    unidad_organizativa=models.ForeignKey(Unidad_Organizativa, on_delete=models.CASCADE)
-    c_contable=models.ForeignKey(Centro_Costo, on_delete=models.CASCADE)
-    provincia=models.CharField(max_length=50)
-    origen=models.CharField(max_length=50)
-    prov_destino=models.CharField(max_length=50)
-    destino=models.CharField(max_length=50)
-    regreso=models.CharField(max_length=50)
-    fecha_inicio=models.DateField()
-    fecha_final=models.DateField()
-    #nuevos models add
+    numero=models.IntegerField() #numero que sale en la columna No (lo asignas  tu en dependencia)
+    solicitante=models.ForeignKey(Trabajador, on_delete=models.CASCADE, related_name='Solicitante') #persona que solicitara el modelo (misma para todas las solicitudes)
+    trabajador=models.ForeignKey(Trabajador, on_delete=models.CASCADE) #trabajador para el cual se hace la solicitud (por supuesto cambia)
+    unidad_organizativa=models.ForeignKey(Unidad_Organizativa, on_delete=models.CASCADE) # misma para todos los trabajadores
+    c_contable=models.ForeignKey(Centro_Costo, on_delete=models.CASCADE) # mismo para todos los trabajadores
+    provincia=models.CharField(max_length=50) ## puedes establecerlo como pred a todos la del primer trabajador pero puede que sea distinta
+    origen=models.CharField(max_length=50) #lo mismo de arriba
+    prov_destino=models.CharField(max_length=50) #lo mismo de arriba
+    destino=models.CharField(max_length=50) #lo mismo de arriba
+    regreso=models.CharField(max_length=50) #lo mismo de arriba
+    ### fechas dietas ###
+    fecha_inicio_dieta=models.DateField()
+    fecha_final_dieta=models.DateField()
+    ### fechas hospedajes ###
+    fecha_inicio_hosp=models.DateField(null=True, blank=True)
+    fecha_final_hosp=models.DateField(null=True, blank=True)
+    ### fechas pasajes ###
+    fecha_inicio_hosp=models.DateField(null=True, blank=True)
+    fecha_final_hosp=models.DateField(null=True, blank=True)
+    ### transportacion###
+    transp_ida = models.CharField(max_length=50, null=True, blank=True)
+    transp_vuelta = models.CharField(max_length=50, null=True, blank=True)
+    ### estos que siguen son lo mismo para el modelo completo, una vez se pongan en la primera solicitud se pone automatico en las demas
     parleg=models.ForeignKey(Trabajador, on_delete=models.CASCADE, related_name='Trabajador_parleg', null=True, blank=True)
     cargo_presupuesto=models.ForeignKey(Cargo_al_Presupuesto, on_delete=models.CASCADE)
     autoriza=models.ForeignKey(Trabajador, on_delete=models.CASCADE, related_name='Autoriza')
@@ -30,3 +40,30 @@ class Solicitud(models.Model):
     class Meta:
         verbose_name="Solicitud"
         verbose_name_plural="Solicitudes"
+
+class Modelo(models.Model):
+    nombre=models.CharField(max_length=50) #persona que crea el modelo
+    solicitante=models.CharField(max_length=50)
+    unidad_organizativa=models.CharField(max_length=100)
+    c_contable=models.CharField(max_length=4)
+    consec=models.IntegerField()
+    solicitudes=models.ManyToManyField(Solicitud)
+    parleg=models.CharField(max_length=200, blank=True, null=True)
+    autoriza=models.CharField(max_length=50)
+    cargo_presupuesto=models.CharField(max_length=20)
+    observaciones = models.CharField(max_length=500, blank=True, null=True)
+    estado=models.CharField(max_length=10)
+    #campos nuevos del autoriza y el solicita
+    cargo_autoriza=models.CharField(max_length=100)
+    dependencia_autoriza=models.CharField(max_length=100)
+    cargo_solicita=models.CharField(max_length=100)
+    area_trabajo_solicita=models.CharField(max_length=100)
+    labor=models.CharField(max_length=500, blank=True, null=True)
+    fecha=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name="Modelo de Solicitudes"
+        verbose_name_plural="Modelos de Solicitudes"
