67b9e3a823bd2f9743abe4a94c2cc35c80fc468b
yandivd <yandivd@gmail.com>
2023-10-13 16:19:48 -0400

    ✨ feat(solicitudes): urls
    urls de los detail view solicitud

--- sgsdph_backend/sistema/api/urls.py
+++ sgsdph_backend/sistema/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .api import solicitud_api_view
+from .api import solicitud_api_view, solicitud_detail_api_view
 
 urlpatterns = [
     path('solicitudes/', solicitud_api_view),
+    path('solicitudes/<int:id>', solicitud_detail_api_view),
 ]
