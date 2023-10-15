e98ec2d4741f5a154ed3623b212c010ed30d494c
yandivd <yandivd@gmail.com>
2023-10-15 12:32:12 -0400

    🐞 fix(soliictud): urls

--- sgsdph_backend/sistema/api/urls.py
+++ sgsdph_backend/sistema/api/urls.py
@@ -3,5 +3,5 @@ from .api import solicitud_api_view, solicitud_detail_api_view
 
 urlpatterns = [
     path('solicitudes/', solicitud_api_view),
-    path('solicitudes/<int:id>', solicitud_detail_api_view),
+    path('solicitudes/<int:id>/', solicitud_detail_api_view),
 ]
