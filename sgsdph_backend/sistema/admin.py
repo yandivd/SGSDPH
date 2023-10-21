2a3f077011882dfd4d9bf80582c3385a8f812230
yandivd <yandivd@gmail.com>
2023-10-21 17:57:57 -0400

    .

--- sgsdph_backend/sistema/admin.py
+++ sgsdph_backend/sistema/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from sistema.models import Solicitud, Persona, Aperitivo, PARLEG
+from sistema.models import Solicitud, Persona, Aperitivo, PARLEG, Modelo
 
 # Register your models here.
 admin.site.register(Solicitud)
+admin.site.register(Modelo)
 admin.site.register(Persona)
 admin.site.register(Aperitivo)
 admin.site.register(PARLEG)
